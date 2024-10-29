78abbee4090ac7c7712f49d0f79ad577dc327b1e
beaprogrammer02345 <108195942+beaprogrammer02345@users.noreply.github.com>
2024-10-29 22:55:07 +0530

    Update schema.ts

--- utils/schema.ts
+++ utils/schema.ts
@@ -1,5 +1,5 @@
 // utils/schema.ts
-import { pgTable, varchar, text, timestamp, serial } from 'drizzle-orm/pg-core';
+import { pgTable, varchar, text, timestamp, serial ,decimal } from 'drizzle-orm/pg-core';
 
 export const aiOutputTable = pgTable('aiOutput', {
   id: serial('id').primaryKey(),
@@ -16,5 +16,7 @@ export const leadTable = pgTable('leads', {
   name: varchar('name', { length: 255 }).notNull(),
   email: varchar('email', { length: 255 }).notNull(),
   templateSlug: text('templateSlug').notNull(), // New field
+  conversionRate: decimal('conversionRate', { precision: 5, scale: 1 }).notNull(), // Added conversion rate field
+  source: varchar('source', { length: 255 }).notNull(), // Added source field
   createdAt: timestamp('createdAt').defaultNow().notNull(),
 });
